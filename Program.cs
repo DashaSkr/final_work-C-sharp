7ce01777045312fc8133e13ee09c340cb7ec14db
Dasha Skr <marybary0707@gmail.com>
2023-06-04 15:47:04 +0300

    program done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+string[] MakeNewString(string[] text)
+{
+    string[] newArray = new string[text.Length -3];
+    for (int i = 0; i < text.Length -3; i++)
+    {
+        newArray[i] = text[i];
+    }
+    return newArray;
+}
+
+string[] array = new string[6] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+Console.Write($" [{String.Join(", ", array)}] ->");
+string[] newArray = MakeNewString(array);
+Console.Write($" [{String.Join(", ", newArray)}]");
+
